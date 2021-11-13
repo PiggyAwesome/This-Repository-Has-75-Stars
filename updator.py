ee9dba34ab80e2ce7004d706cbbd8eb39bacb73f
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2021-11-13 14:44:29 +0200

    Update updator.py

--- updator.py
+++ updator.py
@@ -15,8 +15,8 @@ gh = Github(token)
 
 headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"token {token}"}
 
-repos = requests.get("https://api.github.com/users/PiggyAwesome/repos?per_page=100", headers=headers)
 while True:
+    repos = requests.get("https://api.github.com/users/PiggyAwesome/repos?per_page=100", headers=headers)
     for thingy in repos.json():
         if "this-repository-has" in thingy["name"].lower():
             name = thingy["name"]
@@ -27,6 +27,7 @@ while True:
                 repo = gh.search_repositories(name)[0]
                 repo.update_file("README.md", random.choice(commitMessages), f"# I bet you can't prove me wrong!\n\n{thingy['stargazers_count']} Stars!", sha=sha)
                 repo.edit(name=f"This-Repository-Has-{thingy['stargazers_count']}-Stars")
+                print("Updated!")
             break
     time.sleep(500)
 
