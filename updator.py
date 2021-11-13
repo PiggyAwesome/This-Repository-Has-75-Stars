5b65b0f1e2a55fcbc287e01638ac3d3c9c4e1ae6
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2021-11-13 14:33:24 +0200

    Create updator.py

---
+++ updator.py
@@ -0,0 +1,34 @@
+"""
+Here is the genius program behind this repository
+"""
+
+
+import requests
+import time
+import random
+from github import Github
+
+
+token = ""
+
+gh = Github(token)
+
+headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"token {token}"}
+
+repos = requests.get("https://api.github.com/users/PiggyAwesome/repos?per_page=100", headers=headers)
+while True:
+    for thingy in repos.json():
+        if "this-repository-has" in thingy["name"].lower():
+            name = thingy["name"]
+            print(name, thingy["stargazers_count"])
+            if f"This-Repository-Has-{thingy['stargazers_count']}-Stars" != name:
+                sha = requests.get(f"https://api.github.com/repos/PiggyAwesome/{name}/contents/README.md").json()["sha"]
+                commitMessages = [f"Pretty sure i wrote {thingy['stargazers_count']}", f"omg another mistake? I'm sure i wrote  {thingy['stargazers_count']} last time!", f"Spelling mistake!", f"This repo is weird. Im sure i typed {thingy['stargazers_count']} last time!"]
+                repo = gh.search_repositories(name)[0]
+                repo.update_file("README.md", random.choice(commitMessages), f"# I bet you can't prove me wrong!\n\n{thingy['stargazers_count']} Stars!", sha=sha)
+                repo.edit(name=f"This-Repository-Has-{thingy['stargazers_count']}-Stars")
+            break
+    time.sleep(500)
+
+
+            
